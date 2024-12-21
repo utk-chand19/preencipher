5835a578c6bdb5f2589067736d4cc75992f2132c
utk-chand19 <utkarshchandekar19@gmail.com>
2024-12-22 00:01:39 +0530

    fix: update routing configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
